cf9e71c7450df42543183ac383d109e17fff7989
Francesco Zappa <fzappa@pc93.tpi.uni-jena.de>
2019-12-18 14:42:29 +0100

    Fixed typos and extended parfile template

--- singlepunc_seq.py
+++ singlepunc_seq.py
@@ -2,14 +2,33 @@
 
 import subprocess
 import numpy as np
+import os
 
 steps=10
 
 TEMPLATE = """\
 # singlepunc_seq.py
-bar_b=1
-par_m_plus=1.0
+npoints_A=60
+npoints_B=60
+npoints_phi=40
+par_b=1.e-10
+par_m_plus=1.
+par_P_plus1=0.
+par_P_plus2=0.
+par_P_plus3=0.
+par_S_plus1=0.
+par_S_plus2=0.
 par_S_plus3={spin}
+par_m_minus=1.e-12
+par_P_minus1=0.
+par_P_minus2=0.
+par_P_minus3=0.
+par_S_minus1=0.
+par_S_minus2=0.
+par_S_minus3=0.
+do_solution_file_output=1
+do_bam_file_output=1
+verbose=1
 #
 """
 
@@ -17,12 +36,15 @@ def run(spin):
     """
     Run TwoPuncture.c
     """
+    dir="SinglePuncture"
     parfile = "singlepunc_S{0:.3f}.par".format(s)
-    open(parfile, "w").write(TEMPLATE.format(spin=spin)
-    cmd = "TwoPuncturesRun.x " + parfile
-    return subprocess.call(cmd, shell=True)
+    os.system("mkdir -p {}".format(dir))
+    open("{}/".format(dir) + parfile, "w").write(TEMPLATE.format(spin=spin))
+    cmd = "../TwoPuncturesRun.x " + parfile
+    cwd=dir+"/"
+    return subprocess.call(cmd, cwd=cwd, shell=True)
 
 if __name__ == '__main__':
 
-    spins = np.linespace(0.,0.9, steps)
+    spins = np.linspace(0.,0.9, steps)
     for s in spins: run(s)
